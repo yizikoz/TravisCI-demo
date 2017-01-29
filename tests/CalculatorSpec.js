15b9fde76935a26be64d78e1cc67425f7c39540e
Nir Geier <nirgeier@gmail.com>
2017-01-30 01:01:50 +0200

    Fixed build

--- tests/CalculatorSpec.js
+++ tests/CalculatorSpec.js
@@ -34,31 +34,32 @@ describe( 'calculator', function () {
         } );
       } );
   }
+  /*
+    describe( 'Failures Test#' + i,
+      function () {
+        it( 'should add ' + number1 + ' plus ' + number2 + ' and be equals to ' + ( number1 + number2 ), function () {
+          expect( Calculator.add( number1, number2 ) ).toBe( number1 - number2 );
+        } );
 
-  describe( 'Failures Test#' + i,
-    function () {
-      it( 'should add ' + number1 + ' plus ' + number2 + ' and be equals to ' + ( number1 + number2 ), function () {
-        expect( Calculator.add( number1, number2 ) ).toBe( number1 - number2 );
-      } );
+        it( 'should add ' + 'a' + ' plus ' + number2 + ' and be equals to ' + ( 'a' + number2 ), function () {
+          expect( Calculator.add( 'a', number2 ) ).toBe( 'a' - number2 );
+        } );
 
-      it( 'should add ' + 'a' + ' plus ' + number2 + ' and be equals to ' + ( 'a' + number2 ), function () {
-        expect( Calculator.add( 'a', number2 ) ).toBe( 'a' - number2 );
-      } );
+        it( 'should add ' + number1 + ' plus ' + number2 + ' and be equals to ' + ( number1 + number2 ), function () {
+          expect( Calculator.add( [ number1, number2 ] ) ).toBe( number1 - number2 );
+        } );
 
-      it( 'should add ' + number1 + ' plus ' + number2 + ' and be equals to ' + ( number1 + number2 ), function () {
-        expect( Calculator.add( [ number1, number2 ] ) ).toBe( number1 - number2 );
-      } );
+        it( 'should subtract ' + number1 + ' from ' + number2 + ' and be equals to ' + ( number1 - number2 ), function () {
+          expect( Calculator.sub( number1, number2 ) ).toBe( number1 + number2 );
+        } );
 
-      it( 'should subtract ' + number1 + ' from ' + number2 + ' and be equals to ' + ( number1 - number2 ), function () {
-        expect( Calculator.sub( number1, number2 ) ).toBe( number1 + number2 );
-      } );
+        it( 'should multiply ' + number1 + 'x' + number2 + ' and be equals to ' + ( number1 * number2 ), function () {
+          expect( Calculator.mul( number1, number2 ) ).toBe( number1 - number2 );
+        } );
 
-      it( 'should multiply ' + number1 + 'x' + number2 + ' and be equals to ' + ( number1 * number2 ), function () {
-        expect( Calculator.mul( number1, number2 ) ).toBe( number1 - number2 );
+        it( 'should divide ' + number1 + '/' + number2 + ' and be equals to ' + ( number2 && number1 / number2 ), function () {
+          expect( Calculator.div( number1, number2 ) ).toBe( number2 && number1 * number2 );
+        } );
       } );
-
-      it( 'should divide ' + number1 + '/' + number2 + ' and be equals to ' + ( number2 && number1 / number2 ), function () {
-        expect( Calculator.div( number1, number2 ) ).toBe( number2 && number1 * number2 );
-      } );
-    } );
+      */
 } );
